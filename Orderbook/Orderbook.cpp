7dcccdea5fabb6fcb25953181f4cf84bb191b185
Mouad Lasri <mouadkor20@gmail.com>
2023-11-19 12:29:30 +0900

    Added samePriceOrders to Order struct, created OrderBook class and moved file reading into a separate function

--- Orderbook/Orderbook.cpp
+++ Orderbook/Orderbook.cpp
@@ -5,7 +5,8 @@
 #include <vector>
 #include <unordered_map>
 #include <sstream>
-
+#include <map>
+#include <algorithm>
 
 struct Order {
     int64_t timestamp;
@@ -15,23 +16,72 @@ struct Order {
     std::string category; // NEW, CANCEL, TRADE (Modify)
     double price; 
     int quantity;
+    std::vector<int> samePriceOrders; // Vector of quantity of orders with the same price
+};
+
+class OrderBook
+{
+private:
+    // Key: Price, Value: Vector of Orders (same price) <=> Price Level
+    std::map<double, Order> bids; // Buy orders
+    std::map<double, Order> asks; // Sell orders
+
+public:
+    void processOrder(const Order& order) {
+        
+        if (order.side == "BUY") {
+            // NEW, CANCEL, TRADE (Modify) cases
+            if (order.category == "NEW") {
+                // Case #1: Order at that price doesn't exist in the order book (BID side)
+             
+                // Case #2: Order at that price already exists in the order book, so add it to the samePriceOrders vector 
+            }
+
+            else if (order.category == "CANCEL") {
+                // Add code
+            }
+            else if (order.category == "TRADE") {
+                // Add code                                  
+            }
+        }
+        else if (order.side == "SELL") {
+            // NEW, CANCEL, TRADE (Modify) cases
+            if (order.category == "NEW") {
+                // Case #1: Order at that price doesn't exist in the order book (BID side)
+
+                // Case #2: Order at that price already exists in the order book, so add it to the samePriceOrders vector 
+            }
+
+            else if (order.category == "CANCEL") {
+                // Add code
+            }
+            else if (order.category == "TRADE") {
+                // Add code                                  
+            }
+           
+        }
+    }
+
+
+    void printOrderBook() {
+        // Add code
+    }
 };
 
 
-
-int main() {
-
-    std::ifstream file("SCH.log"); // Replace "your_file.csv" with your file name
+void ReadFile(const std::string& filePath) 
+{
+    std::ifstream file(filePath); // Replace "your_file.csv" with your file name
     if (!file.is_open()) {
         std::cerr << "Error opening file!" << std::endl;
-        return 1;
+        //return 1; // uncomment this line if you want to exit the function on error
     }
 
     int linesToRead = 10; // REMOVE: Read only 10 lines for now for testing purposes
 
     std::string line;
     while (std::getline(file, line) && linesToRead > 0)  // REMOVE: linesToRead > 0 to read the entire file
-    { 
+    {
         std::vector<std::string> tokens;
         std::istringstream tokenStream(line);
         std::string token;
@@ -52,9 +102,9 @@ int main() {
 
             // Process or use the extracted data as needed
             // Example: Print the fields
-            std::cout << "Epoch: " << epoch << ", Order ID: " << orderId << ", Symbol: " << symbol
+            /*std::cout << "Epoch: " << epoch << ", Order ID: " << orderId << ", Symbol: " << symbol
                 << ", Order Side: " << orderSide << ", Order Category: " << orderCategory
-                << ", Price: " << price << ", Quantity: " << quantity << std::endl;
+                << ", Price: " << price << ", Quantity: " << quantity << std::endl;*/
         }
         else {
             std::cerr << "Invalid number of fields in line: " << line << std::endl;
@@ -64,7 +114,13 @@ int main() {
     }
 
     file.close();
- 
-    return 0;
-    
+}
+
+int main() {
+
+    // file path
+    std::string filePath = "SCH.log";
+    ReadFile(filePath);
+
+	return 0;
 }
